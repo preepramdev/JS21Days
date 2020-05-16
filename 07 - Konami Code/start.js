53223e75efac53da183ecb48e785d1fbbaf55764
PRAM0012 <preepram.lea@bru.ac.th>
2020-05-17 02:32:57 +0700

    - day 7

--- 07 - Konami Code/start.js
+++ 07 - Konami Code/start.js
@@ -1,4 +1,33 @@
 (() => {
   // เริ่มเขียนโค้ด
 
+  const konamiCode = [
+    'ArrowUp',
+    'ArrowUp',
+    'ArrowDown',
+    'ArrowDown',
+    'ArrowLeft',
+    'ArrowRight',
+    'ArrowLeft',
+    'ArrowRight',
+    'b',
+    'a'
+  ];
+
+  let index = 0;
+
+  function onKeydown(event) {
+    event.key === konamiCode[index] ? index++ : index = 0;
+    console.log(event);
+
+    if (konamiCode.length === index) {
+      startSnowing();
+    }
+  }
+
+  function run() {
+    document.addEventListener('keydown', onKeydown);
+  }
+
+  run();
 })();
