27a33f649852c73ee2994e77eaa70e6d46c62498
PRAM0012 <preepram.lea@bru.ac.th>
2020-07-01 16:26:16 +0700

    - day 11

--- 11 - Text Reveal/start.js
+++ 11 - Text Reveal/start.js
@@ -1,4 +1,24 @@
 (() => {
   // เริ่มเขียนโค้ด
+  function onScroll() {
+    const selectionElems = Array.from(document.querySelectorAll('section'));
 
+    selectionElems.forEach(selectionElem => {
+      const imgElem = selectionElem.querySelector('img');
+      const textElem = selectionElem.querySelector('.text');
+
+      const revealPosition = imgElem.offsetTop + imgElem.offsetHeight / 10;
+      const scrollPosition = window.pageYOffset;
+
+      if (scrollPosition >= revealPosition) {
+        textElem.classList.add('reveal');
+      }
+    });
+  }
+
+  function run() {
+    document.addEventListener('scroll', onScroll);
+  }
+
+  run();
 })();
